3c4874376d890b30505feb9afe4dcea9b937af42
Jewls135 <juliansw@usca.edu>
2026-03-02 02:12:21 -0500

    Added Mongodb and included it in server startup

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
 import express from "express";
 import cors from "cors";
+
+import connectDB from "./src/config/db.js";
 import apiRoutes from "./src/routes/apiRoutes.js";
 
 const app = express();
+connectDB(); // Initializing db on server start
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  // Default route
-  res.send({ status: "Backend working" });
-});
-
-app.use("/api", apiRoutes);
+app.use("/api", apiRoutes); // All /api routes now automatically require token validation
 
 const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
