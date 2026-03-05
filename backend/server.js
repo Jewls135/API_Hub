0dac35566014e2b5d3c95470a8eda22227b66839
Jewls135 <juliansw@usca.edu>
2026-03-05 02:17:04 -0500

    Updating backend for railway hosting

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 
 app.use("/api", apiRoutes); // All /api routes now automatically require token validation
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
